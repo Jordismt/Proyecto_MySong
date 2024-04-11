1f306d9f982a186be16a88ee3b438dbbf0e3bfb5
Jordi Casanova <jodocasol2002@gmail.com>
2024-04-11 20:01:24 +0200

    primer comit

---
+++ Proyecte_final_DAM/MySong/src/test/java/com/mysong/MySong/MySongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysong.MySong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
